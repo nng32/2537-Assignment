db591059610994bd0dcd396ef316e77d043d7870
Nathan Ng <longcaster19@gmail.com>
2022-05-10 09:37:58 -0700

    Add serverside routes for modifying database

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
-const https = require('https')
+const express = require('express');
+const app = express();
+const https = require('https');
+const mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
+
+mongoose.connect("mongodb://localhost:27017/timelineevents",
+    { useNewUrlParser: true, useUnifiedTopology: true });
+const timelineSchema = new mongoose.Schema({
+    text: String,
+    hits: Number,
+    time: String
+});
+const timelineModel = mongoose.model("timelineevents", timelineSchema);
 
 app.listen(process.env.PORT || 5000, function (err) {
     if (err) console.log(err);
@@ -78,4 +88,56 @@ app.get('/profile/:id', (req, res) => {
             })
         })
     })
+})
+
+app.get('/timeline', (req, res) => {
+    timelineModel.find({}, (err, timelineevents) => {
+        if (err) {
+            console.log("Error " + err);
+        } else {
+            console.log("Data " + JSON.stringify(timelineevents));
+        }
+        res.json(timelineevents);
+    });
+})
+
+app.put("/timeline/insert", (req, res) => {
+    timelineModel.create({
+        text: req.body.text,
+        hits: req.body.hits,
+        time: req.body.time
+    })
+    }, (err, data) => {
+        if (err) console.log(err);
+        else
+            console.log(data);
+        res.send("Insertion complete.")
+});
+
+app.get('/timeline/update/:id', (req, res) => {
+    console.log(req.params.id);
+
+    timelineModel.updateOne({
+        _id: req.params.id
+    }, {
+        $inc: {hits: 1}
+    }), (err, data) => {
+        if (err) console.log(err);
+        else console.log(data);
+        res.send("Hits +1");
+    }
+})
+
+app.get('/timeline/remove/:id', (req, res) => {
+    console.log(req.params.id);
+
+    timelineModel.remove({
+        _id: req.params.id
+    }, {
+        $inc: { hits: 1 }
+    }), (err, data) => {
+        if (err) console.log(err);
+        else console.log(data);
+        res.send("Removed");
+    }
 })
